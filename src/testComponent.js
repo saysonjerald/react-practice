15948d42714f76cb4e385cae356f78e18c34dedf
unknown <saysonjerald@gmail.com>
2021-04-01 17:27:13 +0800

    Making components for the first time

---
+++ src/testComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestComponent = () => {
+    return (
+        <div>
+            <h2>Hello there!</h2>
+            <p>This is me, from the component</p>
+        </div>
+    );
+}
+
+export default TestComponent;
