b35d36bcb981e633c167248404cf240e0c619fe8
Jerald Sayson <saysonjerald@gmail.com>
2021-04-05 22:29:05 +0800

    Added CSS and SASS

--- src/testComponent.js
+++ src/testComponent.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+//options on how to add CSS
+
+//SASS
+import "./styles/App.scss";
+
+//Modulized CSS
+import s from "./styles/App.module.css";
+
 const TestComponent = () => {
     return (
-        <div>
+        <div className="test">
             <h2>Hello there!</h2>
-            <p>This is me, from the component</p>
+            <p className={s.blue}>This is me, from the component</p>
         </div>
     );
 }
